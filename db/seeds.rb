aea69e8ac50288ac334dc913d5be2c20b12afaa9
Kostiantyn <usatov.k.s@gmail.com>
2016-04-11 19:08:29 +0300

    Two homework

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,20 +17,9 @@ Product.create!(title: 'CoffeeScript',
 	shows you how to hold onto all the power and flexibility of JavaScript
 	while writing clearer, cleaner, and safer code.
       </p>},
-  image_url:   'cs.jpg',    
+  image_url:   'http://2.bp.blogspot.com/-bKdgLp-rL3g/UdAnQF7JxaI/AAAAAAAAAJo/FAYc_3yjSNs/s210/coffeescript_logo-553d0e0b9fc0a816ef444280eeabc84d.png',    
   price: 36.00)
 # . . .
-Product.create!(title: 'Programming Ruby 1.9 & 2.0',
-  description:
-    %{<p>
-        Ruby is the fastest growing and most exciting dynamic language
-        out there. If you need to get working programs delivered fast,
-        you should add Ruby to your toolbox.
-      </p>},
-  image_url: 'ruby.jpg',
-  price: 49.95)
-# . . .
-
 Product.create!(title: 'Rails Test Prescriptions',
   description: 
     %{<p>
@@ -43,3 +32,24 @@ Product.create!(title: 'Rails Test Prescriptions',
       </p>},
   image_url: 'rtp.jpg',
   price: 34.95)
+  # . . .
+  Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+  description:
+    %{<p>
+        Ruby is the fastest growing and most exciting dynamic language
+        out there. If you need to get working programs delivered fast,
+        you should add Ruby to your toolbox.
+      </p>},
+  image_url: 'ruby.jpg',
+  price: 49.95)
+  # . . .
+  Product.create!(title: 'Ruby mine',
+  description:
+    %{<p>
+        RubyMine — коммерческая IDE для разработки программного обеспечения на Ruby компании JetBrains.
+		RubyMine создан на основе IntelliJ IDEA того же производителя. 
+		Поддерживает популярные библиотеки, используемые в Ruby-приложениях 
+		(в том числе Bundler, RSpec, Shoulda, Cucumber, Git).
+      </p>},
+  image_url: 'https://upload.wikimedia.org/wikipedia/ru/thumb/2/2e/RubyMine_logo.png/220px-RubyMine_logo.png',
+  price: 79.95)
