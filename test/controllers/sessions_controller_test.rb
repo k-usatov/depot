912fe4e2fb941c857c20cfb1af985325ea2082e4
k-usatov <usatov.k.s@gmail.com>
2016-06-04 18:18:17 +0300

    (14lvl_all)

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionController::TestCase
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should login" do
+	  kostya = users(:one)
+	  posr :create, name: kostya.name, password: 'secret'
+	  assert_redirect_to admin_url
+	  assert_equal kostya.id, session[:user_id]
+  end
+
+  test "should fail login" do
+	  kostya = users(:one)
+	  posr :create, name: kostya.name, password: 'wrong'
+	  assert_redirect_to login_url
+  end
+  
+  test "should logout" do	
+	  delete :destroy
+	  assert_redirect_to store_url
+  end
+end
