b6a1f3b7de18529c632405c130966f13233cd3f4
k-usatov <usatov.k.s@gmail.com>
2016-06-20 10:43:31 +0300

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,26 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+	
+	before_action :set_i18n_locale_from_params
+  # ...
+	protected
+		def set_i18n_locale_from_params
+			if params[:locale]
+				if I18n.available_locales.map(&:to_s).include?(params[:locale])
+					I18n.locale = params[:locale]
+				else
+					flash.now[:notice] ="#{params[:locale]} translation not available"
+											# перевод недоступен
+					logger.error flash.now[:notice]
+				end
+		end
+	end 
+	 
+	def default_url_options
+		{ locale: I18n.locale }
+	end
+	
 	before_action :authorize
 	
 	protected
